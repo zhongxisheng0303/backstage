1ecdf779898951d08bbdc7a097e6bdcac9506018
zhongxisheng0303 <2308021763@qq.com>
2019-07-16 17:41:13 +0800

    axios拦截器完成

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,12 @@
 //导入axios
 import axios from 'axios';
 
+//导入router
+import router from '../router/router';
+
+//导入vue
+import Vue from 'vue';
+
 //创建axios
 const backstage = axios.create({
     //设置基地址
@@ -8,6 +14,35 @@ const backstage = axios.create({
 });
 
 //axios的拦截器
+
+// 添加请求拦截器
+backstage.interceptors.request.use(function (config) {
+    // 在发送请求之前做一些事情
+    //添加token
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config;
+}, function (error) {
+    // 请求失败做一些的事情
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+backstage.interceptors.response.use(function (response) {
+    // 做一些响应回来后做的事情
+    //判断是否是有效的token
+    if(response.data.meta.status == 400 || response.data.meta.msg == "无效token"){
+        //无效删除伪造token
+        window.sessionStorage.clear();
+        //回到登录页
+        router.push('/login');
+        //警告
+        new Vue().$message.error('请勿干一些骚操作,老老实实登录!')
+    }
+    return response;
+}, function (error) {
+    // 做一些响应错误的事情
+    return Promise.reject(error);
+});
 
 //暴露接口 --登录
 export const login = ({ username, password }) => {
@@ -18,9 +53,5 @@ export const login = ({ username, password }) => {
 };
 //暴露接口 --菜单权限
 export const menus = () => {
-    return backstage.get('menus',{
-        headers: {
-            Authorization:window.sessionStorage.getItem('token'),
-        }
-    })
+    return backstage.get('menus')
 };
