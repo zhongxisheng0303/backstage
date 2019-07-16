a760c890eb1aeeb3ebe87eebdb4564cc5ee3df08
zhongxisheng0303 <2308021763@qq.com>
2019-07-16 15:11:23 +0800

    用户管理分页完成

--- src/views/users.vue
+++ src/views/users.vue
@@ -25,6 +25,16 @@
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageIndex"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    ></el-pagination>
   </div>
 </template>
 
@@ -57,9 +67,17 @@ export default {
           date: "2016-05-03",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1516 弄"
-        },
-      ]
+        }
+      ],
+      //当前页
+      pageIndex: 1
     };
+  },
+  //方法
+  methods: {
+    //分页方法
+    handleSizeChange() {},
+    handleCurrentChange() {}
   }
 };
 </script>
